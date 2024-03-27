c7b5f36927ee50fd7f7bf21d0083789cc67913e4
tamnguyen1411 <chuotvip233@gmail.com>
2024-03-27 16:58:09 +0700

    Thêm DAO: :CTHoaDon, CTPhieuNhap, GiamGia

--- src/DAO/CTPhieuNhapDAO.java
+++ src/DAO/CTPhieuNhapDAO.java
@@ -4,10 +4,105 @@
  */
 package DAO;
 
+import java.util.ArrayList;
+import java.sql.*;
+import Model.CTPhieuNhap;
 /**
  *
  * @author ADMIN
  */
 public class CTPhieuNhapDAO {
+    
+    public ArrayList<CTPhieuNhap> getListChiTietPhieuNhap() {
+    ArrayList<CTPhieuNhap> dsChiTietPhieuNhap = new ArrayList<>();
+    try {
+        String sql = "SELECT * FROM ctphieunhap";
+        PreparedStatement ps = MyConnect.conn.prepareStatement(sql);
+       
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            CTPhieuNhap ctpn = new CTPhieuNhap();
+            ctpn.setMaPN(rs.getInt("MaPN"));
+            ctpn.setMaSP(rs.getInt("MaSP"));
+            ctpn.setSoLuong(rs.getInt("SoLuong"));
+            ctpn.setDonGia(rs.getInt("DonGia"));
+            ctpn.setThanhTien(rs.getInt("ThanhTien"));
+            dsChiTietPhieuNhap.add(ctpn);
+        }
+    } catch (SQLException ex) {
+        ex.printStackTrace();
+    }
+    return dsChiTietPhieuNhap;
+}
+    
+    public boolean themChiTietPhieuNhap(CTPhieuNhap ctpn) {
+        boolean result = false;
+        String sql = "INSERT INTO ctphieunhap (MaPN, MaSP, SoLuong, DonGia, ThanhTien) VALUES (?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement pre = MyConnect.conn.prepareStatement(sql);
+            pre.setInt(1, ctpn.getMaPN());
+            pre.setInt(2, ctpn.getMaSP());
+            pre.setInt(3, ctpn.getSoLuong());
+            pre.setInt(4, ctpn.getDonGia());
+            pre.setInt(5, ctpn.getThanhTien());
+            result = pre.executeUpdate() > 0;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return result;
+}
+    
+    public boolean xoaChiTietPhieuNhap(int MaPN) {
+        boolean result = false;
+        String sql = "DELETE FROM ctphieunhap WHERE MaPN = ?";
+        try {
+            PreparedStatement pre = MyConnect.conn.prepareStatement(sql);
+            pre.setInt(1, MaPN);
+            result = pre.executeUpdate() > 0;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return result;
+}
+    public boolean capNhatPhieuNhap(CTPhieuNhap pn) {
+    boolean result = false;
+    String sql = "UPDATE phieunhap SET MaSP=?, SoLuong=?, DonGia=?, ThanhTien=? WHERE MaPN=?";
+    try {
+        PreparedStatement pre = MyConnect.conn.prepareStatement(sql);
+        pre.setInt(1, pn.getMaSP());
+        pre.setInt(2,pn.getSoLuong());
+        pre.setInt(3, pn.getDonGia());
+        pre.setInt(4, pn.getThanhTien());
+        pre.setInt(5, pn.getMaPN());
+        result = pre.executeUpdate() > 0;
+    } catch (SQLException ex) {
+        ex.printStackTrace();
+    }
+    return result;
+}
+
+    public CTPhieuNhap getChiTietPhieuNhap(int maPN) {
+    String sql = "SELECT * FROM ctphieunhap WHERE MaPN = ? ";
+    CTPhieuNhap ctpn = new CTPhieuNhap();
+    try {
+        PreparedStatement pre = MyConnect.conn.prepareStatement(sql);
+        pre.setInt(1, maPN);
+        ResultSet rs = pre.executeQuery();
+        if (rs.next()) {
+            ctpn.setMaPN(rs.getInt("MaPN"));
+            ctpn.setMaSP(rs.getInt("MaSP"));
+            ctpn.setSoLuong(rs.getInt("SoLuong"));
+            ctpn.setDonGia(rs.getInt("DonGia"));
+            ctpn.setThanhTien(rs.getInt("ThanhTien"));
+        }
+    } catch (SQLException ex) {
+        ex.printStackTrace();
+    }
+    return ctpn;
+}
+
+
+
+
     
 }
--- src/DAO/GiamGiaDAO.java
+++ src/DAO/GiamGiaDAO.java
@@ -4,10 +4,71 @@
  */
 package DAO;
 
+import java.util.List;
+import java.sql.*;
+import java.util.List;
+import Model.GiamGia;
+import java.util.ArrayList;
+
 /**
  *
  * @author ADMIN
  */
 public class GiamGiaDAO {
+    public List<GiamGia> getAllGiamGia() {
+        List<GiamGia> list = new ArrayList<>();
+        String sql = "SELECT * FROM giamgia";
+        try {
+            PreparedStatement ps = MyConnect.conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                int maGiam = rs.getInt("MaGiam");
+                String tenGiamGia = rs.getString("TenGiamGia");
+                int phanTramGiam = rs.getInt("PhanTramGiam");
+                int dieuKien = rs.getInt("DieuKien");
+                Date ngayBD = rs.getDate("NgayBD");
+                Date ngayKT = rs.getDate("NgayKT");
+
+                GiamGia giamGia = new GiamGia(maGiam, tenGiamGia, phanTramGiam, dieuKien, ngayBD, ngayKT);
+                list.add(giamGia);
+            }
+            } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return list;
+    }
     
+    
+    
+    public boolean deletePhieuNhap(int maPhieuNhap) {
+        boolean result = false;
+        String sql = "DELETE FROM PhieuNhap WHERE MaPN = ?";
+        
+        try {
+            PreparedStatement ps = MyConnect.conn.prepareStatement(sql);
+            ps.setInt(1, maPhieuNhap);
+            result = ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+      public boolean deleteGiamGia(int maGiam) {
+        boolean result = false;
+        String sql = "DELETE FROM giamgia WHERE MaGiam = ?";
+        try {
+            PreparedStatement ps = MyConnect.conn.prepareStatement(sql);
+            ps.setInt(1, maGiam);
+            result = ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
+      
+     
+      
+        
+    
+
