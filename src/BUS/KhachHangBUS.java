39f7b676f83178a125974e791d5c837f3d106033
hieulag15 <hieuthanhtran12@gmail.com>
2024-03-30 22:53:53 +0700

    Cap nhat NhanVien, KhachHang Bus

--- src/BUS/KhachHangBUS.java
+++ src/BUS/KhachHangBUS.java
@@ -4,6 +4,7 @@
  */
 package BUS;
 
+import Custom.MyDialog;
 import DAO.KhachHangDAO;
 import Model.KhachHang;
 import java.util.ArrayList;
@@ -21,5 +22,77 @@ public class KhachHangBUS {
             this.dskh = khachHangDAO.getDanhSachKhachHang();
         }
         return dskh;
+    }
+    
+    public boolean themKhachHang(String ho, String ten, String gioiTinh) {
+        if (ho.trim().equals("")) {
+            new MyDialog("Không được để trống họ!", MyDialog.ERROR_DIALOG);
+            return false;
+        }
+        if (ten.trim().equals("")) {
+            new MyDialog("Không được để trống tên!", MyDialog.ERROR_DIALOG);
+            return false;
+        }
+        if (gioiTinh.equals("Chọn giới tính")) {
+            new MyDialog("Hãy chọn giới tính!", MyDialog.ERROR_DIALOG);
+            return false;
+        }
+        KhachHang kh = new KhachHang();
+        kh.setHo(ho);
+        kh.setTen(ten);
+        kh.setGioiTinh(gioiTinh);
+        kh.setTongChiTieu(0);
+        boolean flag = khachHangDAO.themKhachHang(kh);
+        if (flag) {
+            new MyDialog("Thêm thành công!", MyDialog.SUCCESS_DIALOG);
+        } else {
+            new MyDialog("Thêm thất bại!", MyDialog.ERROR_DIALOG);
+        }
+        return flag;
+    }
+    
+    public boolean suaKhachHang(String ma, String ho, String ten, String gioiTinh) {
+        if (ho.trim().equals("")) {
+            new MyDialog("Không được để trống họ!", MyDialog.ERROR_DIALOG);
+            return false;
+        }
+        if (ten.trim().equals("")) {
+            new MyDialog("Không được để trống tên!", MyDialog.ERROR_DIALOG);
+            return false;
+        }
+        if (gioiTinh.equals("Chọn giới tính")) {
+            new MyDialog("Hãy chọn giới tính!", MyDialog.ERROR_DIALOG);
+            return false;
+        }
+        KhachHang kh = new KhachHang();
+        kh.setHo(ho);
+        kh.setTen(ten);
+        kh.setGioiTinh(gioiTinh);
+        boolean flag = khachHangDAO.capNhatKhachHang(kh);
+        if (flag) {
+            new MyDialog("Sửa thành công!", MyDialog.SUCCESS_DIALOG);
+        } else {
+            new MyDialog("Sửa thất bại!", MyDialog.ERROR_DIALOG);
+        }
+        return flag;
+    }
+    
+    public boolean xoaKhachHang(String maKh) {
+        boolean flag = false;
+        try {
+            int maKH = Integer.parseInt(maKh);
+            MyDialog dlg = new MyDialog("Bạn có chắc chắn muốn xoá?", MyDialog.WARNING_DIALOG);
+            if(dlg.getAction() == MyDialog.CANCEL_OPTION)
+                return false;
+            flag = khachHangDAO.xoaKhachHang(maKH);
+        } catch (Exception e) {
+            new MyDialog("Chưa chọn khách hàng!", MyDialog.ERROR_DIALOG);
+        }
+        if (flag) {
+            new MyDialog("Xoá thành công!", MyDialog.SUCCESS_DIALOG);
+        } else {
+            new MyDialog("Xoá thất bại!", MyDialog.ERROR_DIALOG);
+        }
+        return flag;
     }
 }
--- src/BUS/NhanVienBUS.java
+++ src/BUS/NhanVienBUS.java
@@ -4,6 +4,7 @@
  */
 package BUS;
 
+import Custom.MyDialog;
 import DAO.NhanVienDAO;
 import Model.NhanVien;
 import java.util.ArrayList;
@@ -16,10 +17,94 @@ public class NhanVienBUS {
     private NhanVienDAO nhanVienDAO = new NhanVienDAO();
     private ArrayList<NhanVien> dsnv = new ArrayList<>();
     
+    public NhanVienBUS() {
+        docDanhSach();
+    }
+
+    public void docDanhSach() {
+        this.dsnv = nhanVienDAO.getDanhSachNhanVien();
+    }
+    
     public ArrayList<NhanVien> getDanhSachNhanVien() {
         if (dsnv == null) {
             this.dsnv = nhanVienDAO.getDanhSachNhanVien();
         }
         return dsnv;
+    }
+    
+    public boolean themNhanVien(String ho, String ten, String gioiTinh, String chucVu) {
+        if (ho.trim().equals("")) {
+            new MyDialog("Họ không được để trống!", MyDialog.ERROR_DIALOG);
+            return false;
+        }
+        if (ten.trim().equals("")) {
+            new MyDialog("Tên không được để trống!", MyDialog.ERROR_DIALOG);
+            return false;
+        }
+        if (chucVu.trim().equals("")) {
+            new MyDialog("Chức vụ không được để trống!", MyDialog.ERROR_DIALOG);
+            return false;
+        }
+        NhanVien nv = new NhanVien();
+        nv.setHo(ho);
+        nv.setTen(ten);
+        nv.setGioiTinh(gioiTinh);
+        nv.setChucVu(chucVu);
+        boolean flag = nhanVienDAO.themNhanVien(nv);
+        if (!flag) {
+            new MyDialog("Thêm thất bại!", MyDialog.ERROR_DIALOG);
+        } else {
+            new MyDialog("Thêm thành công!", MyDialog.SUCCESS_DIALOG);
+        }
+        return flag;
+    }
+
+    public boolean updateNhanVien(String ma, String ho, String ten, String gioiTinh, String chucVu) {
+        int maNV = Integer.parseInt(ma);
+        if (ho.trim().equals("")) {
+            new MyDialog("Họ không được để trống!", MyDialog.ERROR_DIALOG);
+            return false;
+        }
+        if (ten.trim().equals("")) {
+            new MyDialog("Tên không được để trống!", MyDialog.ERROR_DIALOG);
+            return false;
+        }
+        if (chucVu.trim().equals("")) {
+            new MyDialog("Chức vụ không được để trống!", MyDialog.ERROR_DIALOG);
+            return false;
+        }
+        NhanVien nv = new NhanVien();
+        nv.setMaNV(maNV);
+        nv.setHo(ho);
+        nv.setTen(ten);
+        nv.setGioiTinh(gioiTinh);
+        nv.setChucVu(chucVu);
+        boolean flag = nhanVienDAO.suaNhanVien(nv);
+        if (!flag) {
+            new MyDialog("Cập nhập thất bại!", MyDialog.ERROR_DIALOG);
+        } else {
+            new MyDialog("Cập nhập thành công!", MyDialog.SUCCESS_DIALOG);
+        }
+        return flag;
+    }
+    
+    public boolean xoaNhanVien(String ma) {
+        try {
+            int maNV = Integer.parseInt(ma);
+            MyDialog dlg = new MyDialog("Bạn có chắc chắn muốn xoá?", MyDialog.WARNING_DIALOG);
+            boolean flag = false;
+            if (dlg.getAction() == MyDialog.OK_OPTION) {
+                flag = nhanVienDAO.xoaNhanVien(maNV);
+                if (flag) {
+                    new MyDialog("Xoá thành công!", MyDialog.SUCCESS_DIALOG);
+                } else {
+                    new MyDialog("Xoá thất bại!", MyDialog.ERROR_DIALOG);
+                }
+            }
+            return flag;
+        } catch (Exception e) {
+            new MyDialog("Chưa chọn nhân viên!", MyDialog.ERROR_DIALOG);
+        }
+        return false;
     }
 }
